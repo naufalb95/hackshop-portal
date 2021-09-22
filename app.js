234c902671b5c7f0a09abd2fae3b5890db31f07d
fathur-k <fathurkaryim@gmail.com>
2021-09-23 02:14:18 +0700

    add item controller done, but without hook

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'HackShop Portal'});
 });
 
-app.get('/seller', SellerController.showAll);
+app.get('/seller/items', SellerController.showAll);
+
+app.get('/seller/add', SellerController.showAddItemForm);
+app.post('/seller/add', SellerController.createItem);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -10,6 +10,23 @@ class SellerController {
     .then( data => res.render('./seller/index', {item: data}) )
     .catch( err => res.send(err) )
   }
+  static showAddItemForm (req, res) {
+    res.render('addItemForm')
+  }
+  static createItem (req, res) {
+    let { name, price, stock, imageUrl, description} = req.body
+    Item.create({
+      name,
+      price,
+      stock,
+      isActive: true,//isActive filled with hooks
+      imageUrl,
+      UserId: 1,//userId filled with session
+      description
+    })
+    .then( data => res.redirect('/seller/items') )
+    .catch( err => res.send(err) )
+  }
 }
 
 module.exports = SellerController
